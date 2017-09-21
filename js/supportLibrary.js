3983da594908f43ab13108bf68660c8b74b00050
Axel Slättman <axel.slattman@gmail.com>
2017-09-21 18:27:25 +0200

    createNode now has some basic functionality, meant to be inplemented on resource estimation page

--- js/supportLibrary.js
+++ js/supportLibrary.js
@@ -19,3 +19,4 @@ window.onclick = function(event) {
     }
   }
 }
+$(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
